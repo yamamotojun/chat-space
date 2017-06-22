941a3e8b6b8584538f2ceb630d32a3e18698ba8b
yamamotojun <ntrkp26022070271@gmail.com>
2017-06-22 20:41:31 +0900

    chatspace_user_4
    chatspace_user_4

--- app/controllers/messeges_controller.rb
+++ app/controllers/messeges_controller.rb
@@ -1,5 +1,12 @@
   class MessegesController < ApplicationController
 
+    before_action :move_to_sign_up, except: :sign_up
+
     def index
     end
+
+    def move_to_index
+      redirect_to action: :sign_up unless user_signed_in?
+    end
+
   end
