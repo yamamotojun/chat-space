924aff0ece203df145ba0ab803f043f89b630712
yamamotojun <ntrkp26022070271@gmail.com>
2017-06-23 13:20:24 +0900

    chatspace_user6
    chatspace_user6

--- app/controllers/messeges_controller.rb
+++ app/controllers/messeges_controller.rb
@@ -1,12 +1,13 @@
   class MessegesController < ApplicationController
 
-    before_action :move_to_index, except: :index
+    before_action :move_to_index, expect: :index
+    before_action :authenticate_user!, expect: :index
 
     def index
     end
 
     def move_to_index
-      redirect_to action: :sign_up unless user_signed_in?
+      # redirect_to action: :sign_in unless user_signed_in?
     end
 
   end
