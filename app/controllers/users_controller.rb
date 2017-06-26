e5e8e29247e056c53376d1cd706ca27f3cf16e2b
yamamotojun <ntrkp26022070271@gmail.com>
2017-06-26 17:57:33 +0900

    chatspase_user10
    chatspase_user10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
 
  def edit
-user = User.find(params[:id])
+  user = User.find(params[:id])
  end
 
  def update
   @user = User.find(params[:id])
   @user.update(user_params)
-  binding.pry
  end
 
 private
